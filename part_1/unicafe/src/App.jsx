9732658ed074a717d22d8f22d3be836f8bd1184e
Oleg Sychev <sychevoleg@gmail.com>
2025-12-25 20:27:56 +0300

    1.10 complete

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -2,45 +2,7 @@ import {useState} from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
-const Feedback = ({text, count}) => {
-    return (
-        <div>{text} {count}</div>
-    )
-}
-
-const Average = ({good, neutral, bad}) => {
-    const score = {
-        good: 1,
-        neutral: 0,
-        bad: -1
-    }
-
-    const sum = good + neutral + bad
-    let average = 0
-    if (sum !== 0) {
-        average = (score.good * good + score.neutral * neutral + score.bad * bad) / sum
-    }
-
-    return (
-        <div>
-            average {average}
-        </div>
-    )
-}
-
-const Positive = ({good, neutral, bad}) => {
-    const sum = good + neutral + bad
-    let positive = 0
-    if (sum !== 0) {
-        positive = good / sum
-    }
-
-    return (
-        <div>
-            positive {positive}
-        </div>
-    )
-}
+const StatisticLine = ({text, value}) => <div>{text} {value}</div>
 
 const Statistics = ({good, neutral, bad}) => {
     if (good === 0 && neutral === 0 && bad === 0) {
@@ -51,17 +13,25 @@ const Statistics = ({good, neutral, bad}) => {
         )
     }
 
+    const score = {
+        good: 1,
+        neutral: 0,
+        bad: -1
+    }
+
+    const sum = good + neutral + bad
+    const average = (score.good * good + score.neutral * neutral + score.bad * bad) / sum
+    const positive = `${good / sum} %`
+
     return (
         <div>
             <h2>statistics</h2>
-            <Feedback text="good" count={good}/>
-            <Feedback text="neutral" count={neutral}/>
-            <Feedback text="bad" count={bad}/>
-            <div>
-                all {good + neutral + bad}
-            </div>
-            <Average good={good} neutral={neutral} bad={bad}/>
-            <Positive good={good} neutral={neutral} bad={bad}/>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
+            <StatisticLine text="all" value={good + neutral + bad}/>
+            <StatisticLine text="average" value={average}/>
+            <StatisticLine text="positive" value={positive}/>
         </div>
     );
 }
